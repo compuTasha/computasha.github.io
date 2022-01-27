4b134916b4e4b143ae307c8ca8d82e3063dee9bc
compuTasha <mijookim.tasha@gmail.com>
2022-01-27 19:35:42 +0900

    modify config.js

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -53,7 +53,7 @@ module.exports = {
         activity: '블로그 시작',
       },
       {
-        date: '2022.01 ~ 2022.01',
+        date: '2022.01 ~ 2022.02',
         activity: 'AWS Cloud Bootcamp 참여',
       },
     ],
